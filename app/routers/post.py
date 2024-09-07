5f56410c2123c0ef5a2065bb64bc38c0e0ef77c0
Mushahid <mushahid1@hotmail.com>
2024-09-07 23:02:44 +0500

    adding sql joins and updating models

--- app/routers/post.py
+++ app/routers/post.py
@@ -2,6 +2,7 @@ from typing import List, Optional
 from .. import models, schemas, oauth2
 from fastapi import status, HTTPException, Depends, Response, APIRouter
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from ..database import get_db
 
 router = APIRouter(
@@ -9,14 +10,19 @@ router = APIRouter(
     tags=["Posts"]
 )
 
-@router.get("/", response_model=List[schemas.Post])
+@router.get("/", response_model=List[schemas.PostOut])
 def get_posts(db:Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), 
               limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # cursor.execute("""SELECT * FROM posts """)
     # posts =  cursor.fetchall()
     # To get only logged in users post we need to add below filter before .all()
     #.filter(models.Post.owner_id == current_user.id)
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    # posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    
+
+    posts = db.query(models.Post, func.count(models.Votes.post_id).label("votes")).join(
+        models.Votes, models.Votes.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()    
+    
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
@@ -35,13 +41,14 @@ def create_post(post: schemas.PostCreate, db:Session = Depends(get_db), current_
     db.refresh(new_post)
     return new_post
 
-@router.get("/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.PostOut)
 def get_post(id: int, response: Response, db:Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
     # post = cursor.fetchone()
 
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-
+    post = db.query(models.Post, func.count(models.Votes.post_id).label("votes")).join(
+        models.Votes, models.Votes.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()    
+    
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
         # response.status_code = status.HTTP_404_NOT_FOUND
